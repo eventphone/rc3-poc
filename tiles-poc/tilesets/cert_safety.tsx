5c9e32316b49c2cafd7257f4ac25ff53f80d8be1
Sascha Ludwig <sascha@rc5.de>
2020-12-12 04:01:28 +0100

    mikrowelle, tisch collision, smoking area

---
+++ tiles-poc/tilesets/cert_safety.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="cert_safety" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <image source="../../tiles-cert/imgs/tilesheets/cert_safety.png" width="512" height="2048"/>
+</tileset>
